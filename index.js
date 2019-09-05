b8555633e890c3c061b5c452e9583603b7896fc1
mohanaveluk <mohanaveluk@gmail.com>
2019-09-05 14:36:59 -0500

    updating index file

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const path = require('path');
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('<h1>Hellp express</h1>');
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server staeter on port ${PORT}`));
 
